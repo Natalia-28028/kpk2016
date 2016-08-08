1ce6c2dba689453a901e7fa0b7d90501c91c1c3f
Мария <>
2016-08-09 02:45:59 +0300

    Надо довести до ума!

---
+++ task_dm4.py
@@ -0,0 +1,17 @@
+from drawman import *
+from time import sleep
+scale = 100
+
+def f(x):
+    return x**2
+
+x = -2
+y = 0
+dx = 0.1
+while x < 2:
+    dy = f(x + dx) - y
+    x = x + dx
+    y = f(x)
+    shift(dx*scale, dy*scale)
+
+sleep(2)
