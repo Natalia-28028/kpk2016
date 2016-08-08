72938d8655862efb5121f057750913f4bc67d1d9
Мария <>
2016-08-09 01:03:46 +0300

    Чертежник используется как модуль

---
+++ task_dm1.py
@@ -0,0 +1,13 @@
+from drawman import *
+from time import sleep
+
+shift(100, 100)
+shift(100, -100)
+shift(-100, -100)
+shift(-100, 100)
+
+shift(-100, 0)
+shift(0, -100)
+shift(100, 0)
+shift(0, 100)
+sleep()
