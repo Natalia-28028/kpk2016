d08127b54a38e3da15d94a26cb6b9bf57bb23993
Мария <>
2016-08-09 01:11:09 +0300

    Написан Чертежник с использованием ГРИС Черепаха

---
+++ task_dm2.py
@@ -0,0 +1,18 @@
+from drawman import *
+from time import sleep
+scale = 100
+
+def f(x):
+    return x**2
+
+x = 0
+y = 0
+dx = 0.1
+while abs(x*scale) < 500 \
+      and abs(y*scale) < 500:
+    dy = f(x + dx) - y
+    x = x + dx
+    y = f(x)
+    shift(dx*scale, dy*scale)
+
+sleep(20)
