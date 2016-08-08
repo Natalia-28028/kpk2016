33295701d3f22515aca7173cbea39edec6fb6be3
Мария <>
2016-08-09 02:48:13 +0300

    Написан Чертежник с использованием ГРИС Черепаха

--- task_dm4.py
+++ task_dm4.py
@@ -5,10 +5,19 @@ scale = 100
 def f(x):
     return x**2
 
-x = -2
+x = 0
 y = 0
 dx = 0.1
-while x < 2:
+while x < 3:
+    dy = f(x + dx) - y
+    x = x + dx
+    y = f(x)
+    shift(dx*scale, dy*scale)
+
+x = 0
+y = 0
+dx = -0.1
+while x > -3:
     dy = f(x + dx) - y
     x = x + dx
     y = f(x)
